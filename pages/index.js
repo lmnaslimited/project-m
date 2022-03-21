e892c49780820f84c4e6c0dca0af7cc1ccd111c8
srinath1204 <srinathmuniyasamy1204@gmail.com>
2022-03-22 00:04:14 +0530

    tailwind theme configuration added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl  font-light underline">
+    <h1 className="text-3xl text-th-primary-light  font-light underline">
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
+const { pink } = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors')
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        emerald: colors.emerald,
+        fuchsia: colors.fuchsia,
+        pink: colors.pink,
+        teal: colors.teal,
+        'th-background': 'var(--background)',
+        'th-background-secondary': 'var(--background-secondary)',
+        'th-foreground': 'var(--foreground)',
+        'th-primary-dark': 'var(--primary-dark)',
+        'th-primary-medium': 'var(--primary-medium)',
+        'th-primary-light': 'var(--primary-light)',
+        'th-accent-dark': 'var(--accent-dark)',
+        'th-accent-medium': 'var(--accent-medium)',
+        'th-accent-light': 'var(--accent-light)',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
