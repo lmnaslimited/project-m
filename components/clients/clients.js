163cc80d09a3da5307e250af87e66873c53b3a5a
ArunG <arung.sap@gmail.com>
2022-03-22 20:21:56 +0530

    Layout of the home page with components created

---
+++ components/clients/clients.js
@@ -0,0 +1,9 @@
+export default function Clients() {
+    return (
+        <>
+     <div className="my-10">
+            <h1> This is Ashok's Section</h1>
+            </div>
+        </>
+    )
+}
---
+++ components/technologies/technologies.js
@@ -0,0 +1,9 @@
+export default function Technologies() {
+    return (
+        <>
+            <div className="my-10">
+                <h1>This is Harish's Section</h1>
+            </div>
+        </>
+    )
+}
