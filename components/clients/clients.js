c06298094a6b651c1c7c8d637a53afcac18698a5
srinath1204 <srinathmuniyasamy1204@gmail.com>
2022-03-24 21:46:42 +0530

    clients

--- components/clients/clients.js
+++ components/clients/clients.js
@@ -1,9 +1,45 @@
-export default function Clients() {
+import Marquee from "react-fast-marquee";
+import Image from 'next/image'
+export default function brands() {
     return (
-        <>
-     <div className="my-10">
-            <h1> This is Ashok's Section</h1>
-            </div>
-        </>
+        <section className="mx-4 p-3">
+            <Marquee>
+
+                <div className="px-3">
+                    <Image
+                        src='/china.png'
+                        height='100'
+                        width='100'
+                        className="absolute inset-0 w-full h-full object-cover object-center border-gray-200"
+                    />
+                </div>
+                <div className="px-3">
+                    <Image
+                        src='/japan.png'
+                        height='100'
+                        width='100'
+                        className="absolute inset-0 w-full h-full object-cover object-center border-gray-200"
+                    />
+                </div>
+                <div className="px-3">
+
+                    <Image
+                        src='/south-korea.png'
+                        height='100'
+                        width='100'
+                        className="absolute inset-0 w-full h-full object-cover object-center border-gray-200"
+                    />
+                </div>
+                <div className="px-3">
+
+                    <Image
+                        src='/european-union.png'
+                        height='100'
+                        width='100'
+                        className="absolute inset-0 w-full h-full object-cover object-center border-gray-200"
+                    />
+                </div>
+            </Marquee>
+        </section>
     )
 }
