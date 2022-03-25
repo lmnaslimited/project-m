34a9f6c3c2cd2af775d4c3796db92286cdd51daf
srinath1204 <srinathmuniyasamy1204@gmail.com>
2022-03-25 21:45:01 +0530

    draft version of home-heros

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,7 +1,30 @@
 export default function Hero () {
     return (
-        <div className="my-10">
-            <h1>This is Abi's Section</h1>
+        <div>
+    <section class="mb-40">
+      <div class="px-6 py-12 md:px-12 text-gray-800 text-center lg:text-left">
+        <div class="container mx-auto xl:px-32">
+          <div class="grid lg:grid-cols-2 flex items-center">
+            <div class="md:mt-12 lg:mt-0 mb-12 lg:mb-0">
+              <div class="block rounded-lg shadow-lg px-6 py-12 md:px-12 lg:-mr-14">
+                <h1 class="text-4xl md:text-5xl xl:text-6xl text-green-500 font-bold tracking-tight mb-2">Hero Section</h1><span class="text-4xl md:text-5xl xl:text-6xl text-green-500 font-bold tracking-tight mb-10 text-green-800">for our website</span>
+                <a class="inline-block px-7 py-3 mt-10 mb-2 md:mb-0 md:mr-2 bg-green-400 text-black hover:text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-green-800 hover:shadow-lg shadow-2xl transform transition hover:-translate-y-1 focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">CTA Button1</a>
+                <a class="inline-block px-7 py-3  mt-10 bg-transparent text-green-800 font-medium text-sm leading-snug uppercase rounded hover:text-green-500 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none shadow-2xl transform transition hover:-translate-y-1 focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">CTA Button2</a>
+              </div>
+            </div>
+            <div class="md:mb-12 lg:mb-0">
+              <img
+                src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
+                class="w-full rounded-lg shadow-lg"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
+      </div>
+    </section>
+  
+  </div>
     )
+
 }
