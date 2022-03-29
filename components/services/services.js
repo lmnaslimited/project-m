6a675480fc5b2ed7a5b57d428a421f633699b7e8
ArunG <arung.sap@gmail.com>
2022-03-29 12:37:45 +0530

    added images for services section

--- components/services/services.js
+++ components/services/services.js
@@ -1,7 +1,76 @@
-export default function Services () {
+export default function Services() {
     return (
         <>
-         <h1>This is Sevika's Section</h1>
+            <section classNameName="text-gray-600 body-font">
+                <div className="container px-5 py-12 mx-auto">
+                    <div className="flex flex-wrap -m-4">
+                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a className="block h-48 rounded overflow-hidden border-2 justify-center">
+                                <img alt="ecommerce" className="object-cover object-center w-40 h-40 block" src="/services/sap-512x512.png" />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">SAP</h3>
+                               
+                            </div>
+                        </div>
+                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a className="block relative h-48 rounded overflow-hidden border-2">
+                                <img alt="ecommerce" className="object-cover object-center w-36 h-36 block" src="/services/consulting-512x512.png" />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">IT CONSULTING</h3>
+                               
+                            </div>
+                        </div>
+                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a className="block relative h-48 rounded overflow-hidden border-2">
+                                <img alt="is-oil" className="object-cover object-center w-36 h-36 block" src="/services/is-oil-512x512.png" />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">SAP IS-OIL</h3>
+                               
+                            </div>
+                        </div>
+                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a className="block relative h-48 rounded overflow-hidden border-2">
+                                <img alt="ecommerce" className="object-cover object-center w-36 h-36 block" src="/services/talent-512x512.png" />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TALENT SOURCING</h3>
+                                
+                            </div>
+                        </div>
+                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-36 h-36 block" src="/services/migration-512x512.png" />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">MIGRATION</h3>
+                               
+                            </div>
+                        </div>
+                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-36 h-36 block" src="/services/cloud-512x512.png" />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HANA CLOUD</h3>
+                               
+                            </div>
+                        </div>
+                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-36 h-36 block" src="/services/support-512x512.png" />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">SUPPORT</h3>
+                               
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
