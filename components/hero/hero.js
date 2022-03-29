0413c8954ac0c3f516d77e5c0fd32f2e94b594ba
srinath1204 <srinathmuniyasamy1204@gmail.com>
2022-03-29 13:06:28 +0530

    draft version of hero section

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,30 +1,28 @@
 export default function Hero () {
     return (
-        <div>
-    <section class="mb-40">
-      <div class="px-6 py-12 md:px-12 text-gray-800 bg-gradient-to-r from-lime-500 to-lime-300 text-center lg:text-left">
-        <div class="container mx-auto xl:px-32">
-          <div class="grid lg:grid-cols-2 flex items-center">
-            <div class="md:mt-12 lg:mt-0 mb-12 lg:mb-0">
-              <div class="block rounded-lg shadow-2xl transform transition hover:-translate-y-4 bg-gray-100 shadow-lg px-6 py-12 md:px-12 lg:-mr-14">
-                <h1 class="text-4xl md:text-5xl xl:text-6xl text-green-500 font-bold tracking-tight mb-2">Hero Section</h1><span class="text-4xl md:text-5xl xl:text-6xl text-green-500 font-bold tracking-tight mb-10 text-green-800">for our website</span>
-                <a class="inline-block px-7 py-3 md:mr-40 lg:mr-1 mr-1 mt-10 mb-2 md:mb-0 md:mr-2 bg-green-400 text-black hover:text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-green-800 hover:shadow-lg shadow-2xl transform transition hover:-translate-y-1 focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">CTA Button1</a>
-                <a class="inline-block px-7 py-3  mt-10 bg-transparent text-green-800 font-medium text-sm leading-snug uppercase rounded hover:text-green-500 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none shadow-2xl transform transition hover:-translate-y-1 focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">CTA Button2</a>
+      <div class="py-16 bg-gray-300 my-5 mx-5">
+      <div class="container m-auto px-6">
+    
+       <div class="lg:flex justify-between items-center">
+           <div class="lg:w-6/12 lg:p-0 p-7">
+              <h1 class="text-4xl mx-20 font-bold leading-tight mb-2 capitalize">  Tailwind Website theme </h1>
+              <h1 class="text-3xl mx-20 font-bold leading-tight mb-5 capitalize"> designed for Clients. </h1>
+              <p class="text-lg mx-20">  With Tailwind you can optimized </p>
+              <p class="text-lg mx-20">customization process. </p>
+    
+              <div class="py-5">
+                   <a href="#" class=" mx-20 text-white rounded-full py-2 px-5 text-lg font-semibold bg-purple-600 inline-block border border-purple-600 mr-3">Try for free</a>
+                   <a href="#" class="text-black rounded-full py-2 px-5 text-lg font-semibold bg-gray-400 inline-block border hover:bg-white hover:text-black">Requist a demo</a>
               </div>
+    
             </div>
-            <div class="md:mb-12 lg:mb-0">
-              <img
-                src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg"
-                class="w-full rounded-lg shadow-2xl transform transition hover:-translate-y-6"
-                alt=""
-              />
+            <div class="mr-40 mt-10 ring-offset-2 ring-4 rounded-full">
+              <img src="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80" class=" w-60 h-60 rounded-full"/>
             </div>
-          </div>
         </div>
+    
       </div>
-    </section>
-  
-  </div>
+    </div>
     )
 
 }
