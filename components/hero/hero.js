720cc710ba5d322cec331ee79a0ee9ce0d449d66
srinath1204 <srinathmuniyasamy1204@gmail.com>
2022-03-31 13:57:16 +0530

    draft version of home-hero

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,53 +1,68 @@
 export default function Hero() {
   return (
+  <section className="leading-normal tracking-normal text-indigo-400 m-6 bg-cover bg-fixed bg-[url('/header.png')] ">
+  <div className="h-full">
+    <div className="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+    
+      <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+        <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+          Main
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+            Hero Message
+          </span>
+          to sell yourself!
+        </h1>
+        <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
+          Sub-hero message, not too long and not too short. Make it just right!
+        </p>
 
+        <form className="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-blue-300 py-2 font-bold mb-2" for="emailaddress">
+              Signup for our newsletter
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+              id="emailaddress"
+              type="text"
+              placeholder="you@somewhere.com"
+            />
+          </div>
 
-    <div className="h-auto bg-gradient-to-r from-lime-900 via-lime-400 to-lime-900">
-      <div className="grid lg:grid-rows-1 lg:grid-cols-2 md:grid-rows-2 md:grid-cols-2 grid-rows- gap-4">
-        <div className="lg:row-span-1 lg:col-span-1 md:row-span-1 md:col-span-2 mx-10 my-10">
-          <h1 className="my-4 text-2xl md:text-3xl lg:text-4xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
-            Main Hero Message
-            <span className="text-white md:text-3xl lg:text-4xl md:inline md:text-center md:text-5xl">
-              <br></br> to sell yourself!
-            </span>
-          </h1>
-          <p className="leading-normal text-base md:text-2xl text:xl font-semibold text-gray-800 text-center md:text-left">
-            Sub-hero message, not too long and </p>
-          <p className="leading-normal text-base md:text-2xl text-xl font-semibold mb-8 text-gray-800 text-center md:text-left">not too short. Make it just right!
-          </p>
-
-          <form className="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
-            <div className="mb-4">
-              <label className="block text-blue-300 py-2 font-bold mb-2" for="emailaddress">
-                Signup for our newsletter
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
-                id="emailaddress"
-                type="text"
-                placeholder="you@somewhere.com"
-              />
-            </div>
-
-            <div className="flex items-center justify-between pt-4">
-              <button
-                className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
-                type="button"
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="lg:row-span-1 lg:col-span-1 md:row-span-1 md:col-span-2 w-full md:pt-40  overflow-hidden">
-          <img className="mx-auto w-full md:w-4/5 transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6" src="https://airwallpaper.com/wp-content/uploads/wall003/Backgrounds-Grey.jpg" /> </div>
+          <div className="flex items-center justify-between pt-4">
+            <button
+              className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+              type="button"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="flex justify-center">
-        <a className="inline-block md:px-7 md:py-3 p-2 mb-2 md:mb-10 bg-purple-600 hover:text-lime-600   mt-10  text-gray-100 font-medium text-sm leading-snug uppercase rounded     hover:bg-gray-200 focus:bg-gray-100 focus:outline-none shadow-2xl transform transition hover:-translate-y-1 focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">CTA Button2</a>
-        <a className="inline-block md:px-7 md:py-3 p-2 mb-2 md:mb-10 bg-purple-600 hover:text-lime-600 ml-10 mt-10  text-gray-100 font-medium text-sm leading-snug uppercase rounded hover:bg-gray-200 focus:bg-gray-100 focus:outline-none shadow-2xl transform transition hover:-translate-y-1 focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">CTA Button2</a>
+
+      
+      <div className="w-full xl:w-3/5 p-12  overflow-hidden">
+        <img className="mx-auto w-full md:w-4/5  transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6" src="/hero-bg-sec.webp"/>
+      </div>
+
+      <div className="mx-auto md:pt-16">
+        <p className="text-blue-400 font-bold pb-8 lg:pb-6 text-center">
+          Download our app:
+        </p>
+        <div className="flex w-full justify-center md:justify-start pb-24 lg:pb-0 fade-in">
+          <img src="App Store.svg" className="h-12 pr-12 transform hover:scale-125 duration-300 ease-in-out" />
+          <img src="Play Store.svg" className="h-12 transform hover:scale-125 duration-300 ease-in-out" />
+        </div>
+      </div>
+
+     
+      <div className="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
+        <a className="text-gray-500 no-underline hover:no-underline" href="#"></a>
+        <a className="text-gray-500 no-underline hover:no-underline" href="https://www.tailwindtoolbox.com"></a>
       </div>
     </div>
-
+  </div>
+</section>
   )
 
 }
