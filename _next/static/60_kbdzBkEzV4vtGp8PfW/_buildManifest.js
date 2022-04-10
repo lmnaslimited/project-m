aa09cdfe04d2e725ec1ff170e7a20c538ca46ee3
ArunG <arung.sap@gmail.com>
2022-04-10 17:56:16 +0530

    Deploy Next.js to gh-pages

--- _next/static/60_kbdzBkEzV4vtGp8PfW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/872-045575bfa076832b.js","static/chunks/pages/index-daf41fd878d519a8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
